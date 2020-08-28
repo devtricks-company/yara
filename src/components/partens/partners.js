b76436c0dbbdcaead96913b3cdf1bac53dec8f76
devtricks-company <amir.mo.azarbashi@gmail.com>
2020-08-28 23:59:35 +0430

    add Section to project

--- src/components/partens/partners.js
+++ src/components/partens/partners.js
@@ -1,18 +1,14 @@
-import React from 'react';
-
-
+import React from "react"
 
 const Partners = () => {
-    return (
-       <section className="partners">
-           <div className="container-fluid">
-            <h2>Partners And Clients</h2>
-            <div className="image-container">
-               
-            </div>
-           </div>
-       </section>
-    )
+  return (
+    <section className="partners">
+      <div className="container-fluid">
+        <h2>Partners And Clients</h2>
+        <div className="image-container"></div>
+      </div>
+    </section>
+  )
 }
 
 export default Partners
